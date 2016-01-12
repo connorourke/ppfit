345f06c39857f2ef12ab5b8e2dc294ceec3449dc
Benjamin Morgan <benmorgan2@gmail.com>
2016-01-12 12:27:42 +0000

    Added iteration counter in SumOfChi.evaluate()

--- ppfit/chi.py
+++ ppfit/chi.py
@@ -67,6 +67,7 @@ class sumOfChi:
     self.ai_forces = training_set.forces.T
     self.ai_dipoles = training_set.dipoles.T
     self.ai_stresses = training_set.stresses.T
+    self.times_called = 0
 
   def evaluate( self, test_values, plot = False ):
     if type( test_values ) is not np.ndarray:
@@ -80,6 +81,7 @@ class sumOfChi:
     ff_forces = self.training_set.new_forces.T
     ff_dipoles = self.training_set.new_dipoles.T
     ff_stresses = self.training_set.new_stresses.T
+    self.times_called += 1
 
     chiSq = {}
     chiSq[ 'forces' ]   = chi_squared( self.ai_forces, ff_forces, plot, 'forces-errors' )
@@ -87,8 +89,9 @@ class sumOfChi:
     chiSq[ 'stresses' ] = chi_squared( self.ai_stresses, ff_stresses, plot, 'stresses-errors' )
     factorTot = sum( self.scaling.values() )
     totalChi = sum( [ self.scaling[ k ] * chiSq[ k ] for k in chiSq.keys() ] ) / factorTot
-
-    output( 'Forces chi sq: ' + fmt.format(chiSq[ 'forces' ])+'\n')
+ 
+    output( 'iteration: ' + str( self.times_called )
+    output( 'Forces chi sq: ' + fmt.format(chiSq[ 'forces' ]) + '\n')
     output( 'Dipoles chi sq: ' + fmt.format(chiSq[ 'dipoles' ]) +'\n')
     output( 'Stresses chi sq: ' + fmt.format(chiSq[ 'stresses' ]) + '\n')
     output( 'Total chi sq (no factors): ' + fmt.format( np.mean( list( chiSq.values() ) ) ) + '\n' )
